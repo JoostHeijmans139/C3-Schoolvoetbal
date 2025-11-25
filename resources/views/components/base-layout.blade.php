5c8fb584adf939e6a36faa6f24a4c68b07255151
JoostHeijmans232554 <d313111@edu.curio.nl>
2025-11-25 11:41:25 +0100

    added font

--- resources/views/components/base-layout.blade.php
+++ resources/views/components/base-layout.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset("css/main.css") }}" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     <title>Home</title>
 </head>
 
@@ -16,7 +19,7 @@
         </div>
         <nav>
             <div class="loginButton">
-                <p>login</p>
+                <h3>login</h3>
             </div>
         </nav>
     </header>
