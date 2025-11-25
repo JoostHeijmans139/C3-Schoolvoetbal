0b1a7ef935fec583c4bed6112e5ba5a97783ddf7
HyperNeutron <nigel.koremans@gmail.com>
2025-11-25 14:58:20 +0100

    updated to include pivot table

--- app/Models/Tournament.php
+++ app/Models/Tournament.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Tournament extends Model
 {
@@ -21,5 +22,10 @@ class Tournament extends Model
         return [
             'start_date' => 'date',
         ];
+    }
+
+    public function teams(): HasMany
+    {
+        return $this->hasMany(Team::class);
     }
 }
