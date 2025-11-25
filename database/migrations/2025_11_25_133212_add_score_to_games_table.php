1988c208e732d90074b47a9e9657b7d44a7df8a4
HyperNeutron <nigel.koremans@gmail.com>
2025-11-25 14:56:34 +0100

    Create 2025_11_25_133212_add_score_to_games_table.php

---
+++ database/migrations/2025_11_25_133212_add_score_to_games_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->integer("score_team_1")->nullable();
+            $table->integer("score_team_2")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->dropColumn(["score_team_1", "score_team_2"]);
+        });
+    }
+};
