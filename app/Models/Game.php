28724663442efc45c5f32b335f6220f6965048ae
HyperNeutron <nigel.koremans@gmail.com>
2025-11-25 14:17:47 +0100

    added models

---
+++ app/Models/Game.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Game extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'start',
+        'duration_in_minutes',
+        'team_1_id',
+        'team_2_id',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'start' => 'datetime',
+        ];
+    }
+
+    public function team1(): BelongsTo
+    {
+        return $this->belongsTo(Team::class, 'team_1_id');
+    }
+
+    public function team2(): BelongsTo
+    {
+        return $this->belongsTo(Team::class, 'team_2_id');
+    }
+}
---
+++ app/Models/Team.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Team extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'location',
+        'user_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function players(): HasMany
+    {
+        return $this->hasMany(Player::class);
+    }
+
+    public function homeMatches(): HasMany
+    {
+        return $this->hasMany(Game::class, 'team_1_id');
+    }
+
+    public function awayMatches(): HasMany
+    {
+        return $this->hasMany(Game::class, 'team_2_id');
+    }
+}
