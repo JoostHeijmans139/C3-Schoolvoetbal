91a6039cc13924e9593c7cb918e53559b2ae2aeb
Nigel Koremans <nigel.koremans@gmail.com>
2025-12-08 10:21:56 +0100

    added team creation page

---
+++ resources/views/teams/create.blade.php
@@ -0,0 +1,35 @@
+<x-base-layout>
+    <h1>Team aanmaken</h1>
+    <form class="createTeamForm" action="{{ route('team.store') }}" method="POST">
+        @csrf
+        @error("teamname")
+        <div>{{ $message }}</div>
+        @enderror
+        <div class="formGroup">
+            <label for="name">Teamnaam</label>
+            <input type="text" id="name" name="name" value="{{ old('name') }}">
+        </div>
+        @error("location")
+        <div>{{ $message }}</div>
+        @enderror
+        <div class="formGroup">
+            <label for="location">Plaats</label>
+            <input type="text" id="location" name="location" value="{{ old('location') }}">
+        </div>
+        @error("players.*")
+        <div>{{ $message }}</div>
+        @enderror
+        <table>
+            <thead>
+                <th><button id="itemButton" type="button">+</button></th>
+                <th>Naam</th>
+                <th>rugnummer</th>
+            </thead>
+            <tbody id="players">
+            </tbody>
+        </table>
+        <input type="submit" value="Team aanmaken">
+    </form>
+
+    <script src="{{ asset('js/player.js') }}" onload="OnLoad()"></script>
+</x-base-layout>
