f0171b2759bb3acab6ff3fca1b92c325f5c5c507
Nigel Koremans <nigel.koremans@gmail.com>
2025-12-15 10:17:06 +0100

    added update logic

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -77,7 +77,47 @@ class TeamController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+
+        $request->validate([
+            "teamname" => "required|string",
+            "location" => "required|string",
+            "players" => "required|array",
+            "players.*" => "required|array",
+            "players.*.id" => "integer",
+            "players.*.name" => "required|string",
+            "players.*.shirt_number" => "required|integer|min:1|max:99",
+        ]);
+
+        $team = Team::findOrFail($id);
+
+        $team->name = $request->teamname;
+        $team->location = $request->location;
+        $team->save();
+
+        $players = $team->players;
+
+        $playerIds = collect($request->players)->pluck('id')->filter()->toArray();
+
+        $playersToDelete = $players->whereNotIn('id', $playerIds);
+
+        foreach($playersToDelete as $deletedPlayer){
+            $deletedPlayer->delete();
+        }
+
+        foreach($request->players as $player){
+            if(isset($player['id'])){
+                $playerOld = Player::findOrFail($player['id']);
+                $playerOld->name = $player['name'];
+                $playerOld->shirt_number = $player['shirt_number'];
+                $playerOld->save();
+            }
+            else{
+                $player['team_id'] = $team->id;
+                Player::insert($player);
+            }
+        }
+
+        return redirect()->route("dashboard.teams");
     }
 
     /**
