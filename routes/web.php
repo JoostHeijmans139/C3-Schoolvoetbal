6ee0033af7f4e5c2424e0a2fa976e0ed0b5e24df
Nigel Koremans <nigel.koremans@gmail.com>
2025-12-15 10:12:14 +0100

    added route for teams dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TeamController;
 use App\Http\Controllers\tournamentController;
@@ -14,10 +15,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 Route::get('/team/create', [TeamController::class, 'create'])->name('team.create');
 Route::post('/team/create', [TeamController::class, 'store'])->name('team.store');
 
+    Route::get('/dashboard/teams', [DashboardController::class, 'teams'])->name('dashboard.teams')->middleware("admin");
+});
 
 require __DIR__ . '/auth.php';
