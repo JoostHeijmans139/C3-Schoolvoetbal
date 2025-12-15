cbf568e38e12a5a453a7c1235444646d1a387038
Nigel Koremans <nigel.koremans@gmail.com>
2025-12-15 10:11:02 +0100

    refactor

--- public/js/player.js
+++ public/js/player.js
@@ -6,48 +6,6 @@ const fields = [
 
 var playerCount = 0;
 
-function OnLoad() {
-    NewItem();
-    document.getElementById("itemButton").onclick = NewItem;
-}
-
-function NewItem(name, shirt_number) {
-    let player = document.createElement("tr");
-
-    player.classList += "player";
-
-    for (let i = 0; i < fields.length; i++) {
-        let td = document.createElement("td");
-        let input = document.createElement("input");
-
-        input.setAttribute("type", fields[i][2]);
-        input.setAttribute("name", "    players[" + playerCount + "][" + fields[i][0] + "]");
-        input.setAttribute("placeholder", fields[i][1]);
-        input.setAttribute("value", fields[i][1]);
-
-        if (fields[i][0] == "name") {
-            input.setAttribute("value", name)
-        };
-        if (fields[i][0] == "shirt_number") {
-            input.setAttribute("value", shirt_number)
-        };
-
-        if (fields[i][2] == "button") {
-            input.setAttribute("value", fields[i][1])
-            input.onclick = function (e) { DeleteThis(e); };
-        };
-
-        td.appendChild(input);
-        player.appendChild(td);
-    }
-
-    document.getElementById("players").appendChild(player);
-
-    playerCount++
-
-    player.scrollIntoView();
-}
-
 function NewItem() {
     let player = document.createElement("tr");
 
