06aba8026aeaa035839222a1af094a7811ea2c2e
Annvka <D273734@edu.curio.nl>
2025-12-17 16:32:32 +0100

    fixed identation

--- resources/views/dashboard/tournaments.blade.php
+++ resources/views/dashboard/tournaments.blade.php
@@ -7,23 +7,24 @@
         <div class="newTournament">
             <a href="{{ route('dashboard.createTournament') }}" class="tournamentButton">Nieuw toernooi</a>
         </div>
-            <table>
-                <thead>
-                    <th>Toernooi</th>
-                    <th>Datum</th>
-                    <th>Locatie</th>
-                    <th></th>
-                </thead>
-                <tbody>
-                    @foreach ($tournaments as $tournament)
-                        <tr>
-                            <td>{{ $tournament->name }}</td>
-                            <td>{{ $tournament->start_date->format('j-n-Y') }}</td>
-                            <td>{{ $tournament->location }}</td>
-                            <td><a href="{{ route('dashboard.tournamentDetails', $tournament->id) }}" class="view">View</a></td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+        
+        <table>
+            <thead>
+                <th>Toernooi</th>
+                <th>Datum</th>
+                <th>Locatie</th>
+                <th></th>
+            </thead>
+            <tbody>
+                @foreach ($tournaments as $tournament)
+                    <tr>
+                        <td>{{ $tournament->name }}</td>
+                        <td>{{ $tournament->start_date->format('j-n-Y') }}</td>
+                        <td>{{ $tournament->location }}</td>
+                        <td><a href="{{ route('dashboard.tournamentDetails', $tournament->id) }}" class="view">View</a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </x-base-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,25 +3,25 @@
     <div class="backgroundColor">
         <a href="{{ route('team.create') }}" class="adminButtons">Eigen team aanmaken</a>
 
-            <table class="teamsTable">
-                <thead>
-                    <th>Locatie</th>
-                    <th>Tijd</th>
-                    <th>Datum</th>
-                    <th>Team 1</th>
-                    <th>Team 2</th>
-                </thead>
-                <tbody>
-                    @foreach ($games as $game)
-                        <tr>
-                            <td></td>
-                            <td>{{ $game->start->format('H:i') }}</td>
-                            <td>{{ $game->start->format('j-n-Y') }}</td>
-                            <td>{{ $game->team1()->get()[0]->name }}</td>
-                            <td>{{ $game->team2()->get()[0]->name }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+        <table class="teamsTable">
+            <thead>
+                <th>Locatie</th>
+                <th>Tijd</th>
+                <th>Datum</th>
+                <th>Team 1</th>
+                <th>Team 2</th>
+            </thead>
+            <tbody>
+                @foreach ($games as $game)
+                    <tr>
+                        <td></td>
+                        <td>{{ $game->start->format('H:i') }}</td>
+                        <td>{{ $game->start->format('j-n-Y') }}</td>
+                        <td>{{ $game->team1()->get()[0]->name }}</td>
+                        <td>{{ $game->team2()->get()[0]->name }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </x-base-layout>
